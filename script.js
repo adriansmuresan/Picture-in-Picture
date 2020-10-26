ae0fd72feb0d64e1d0b70f79d60f82d6cfe93432
Adrian S MUresan <adriansmuresan@gmail.com>
2020-10-26 15:02:17 -0500

    Declares the variables, creates the function

---
+++ script.js
@@ -0,0 +1,15 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+
+  } catch (error) {
+    //  Catch error
+    console.log('whoops, error here:', error);
+  }
+}
+
+//  On load
+selectMediaStream();
